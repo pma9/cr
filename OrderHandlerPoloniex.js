a313e0d5351843574ae9dff6ff5c1b4a325e5449
jeffsenk <jeff.senk@gmail.com>
2016-09-05 19:24:31 -0500

    key removal

--- OrderHandlerPoloniex.js
+++ OrderHandlerPoloniex.js
@@ -1,5 +1,10 @@
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('/home/jeff/api/apiSettings.ini');
 var Poloniex = require('poloniex-unofficial');
-var poloniex = new Poloniex.TradingWrapper('1Q0BJ6CX-QESJ00ND-3NOAYKKE-GLX6B8J3','9915a3cbcf751e3bb619c98cee19b3c448cfe76adae58248f849c1c78bc7f28d61852ba1d2cfba5734214eb5b12952606f56efe35fc2546dfd937a2027f62470',function(){
+var key = properties.get('data.key');
+var secret = properties.get('data.secret');
+
+var poloniex = new Poloniex.TradingWrapper(key,secret,function(){
   return Date.now()*1000;
 });
 var inherits = require('util').inherits;
