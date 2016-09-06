1d431398f8b6ab1bb7d046343e3a7653223d3b5d
jeffsenk <jeff.senk@gmail.com>
2016-09-05 20:13:56 -0500

    first draft algo

---
+++ Level.js
@@ -0,0 +1,7 @@
+
+function Level(distance,takeProfit,stopout){
+  this.distance = distance;
+  this.takeProfit = takeProfit;
+  this.stopOut = stopOut;
+}
+module.exports = Level;
--- PoloniexProd.js
+++ PoloniexProd.js
@@ -1,15 +1,14 @@
-var PropertiesReader = require('properties-reader');
-var properties = PropertiesReader('logSettings.ini');
 var product = process.argv[2];
+var PropertiesReader = require('properties-reader');
+var properties = new PropertiesReader(product+'.ini');
 var Handler = require('./MktDataHandlerPoloniex');
 var handler = new Handler(product);
 var DataReader = require('./DataReaderPoloniexProd');
 var reader = new DataReader(handler);
 var OrderBookMgr = require('./OrderBookMgrPoloniex');
 var orderBookMgr = new OrderBookMgr(reader);
-
-orderBookMgr.on('askUpdate',function(data){
-  console.log(data);
-});
+var MktMakeAlgo = require('./MktMakeAlgo');
+var orderMgr = {};
+var algo = new MktMakeAlgo(properties,orderBookMgr, orderMgr);
 
 handler.run();
