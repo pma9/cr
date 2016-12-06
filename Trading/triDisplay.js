46ef77b04572042b9c3606c61067a64f9fb8b02f
jeffsenk <jeff.senk@gmail.com>
2016-12-06 16:33:50 -0600

    commit printing cross data

--- Trading/triDisplay.js
+++ Trading/triDisplay.js
@@ -6,9 +6,11 @@ var orderBookMgrBFNX = new OrderBookMgrBFNX(handlerBFNX);
 var HandlerGDAX = require('../DataHandling/TriMktDataHandlerGDAX');
 var handlerGDAXBTC = new HandlerGDAX('BTC-USD');
 var handlerGDAXETH = new HandlerGDAX('ETH-USD');
+var handlerGDAXETHBTC = new HandlerGDAX('ETH-BTC');
 var OrderBookMgrGDAX = require('../Trading/TriOrderBookMgrGDAX');
 var orderBookMgrGDAXBTC = new OrderBookMgrGDAX(handlerGDAXBTC,'BTC-USD');
 var orderBookMgrGDAXETH = new OrderBookMgrGDAX(handlerGDAXETH,'ETH-USD');
+var orderBookMgrGDAXETHBTC = new OrderBookMgrGDAX(handlerGDAXETHBTC,'ETH-BTC');
 
 var bidBFNX = 0;
 var askBFNX = 0;
@@ -16,14 +18,29 @@ var bidGDAXBTC = 0;
 var askGDAXBTC = 0;
 var bidGDAXETH = 0;
 var askGDAXETH = 0;
+var bidGDAXETHBTC = 0;
+var askGDAXETHBTC = 0;
 var innerMkt = 0;
 var outerMkt = 0;
+var impBid = 0;
+var impAsk = 0;
+
+function bidETHBTC(BTCshort,USDlong,BTClong){
+  return (USDlong / BTClong) - BTCshort;
+}
+
+function askETHBTC(BTClong,USDlong,ETHlong){
+  return (BTClong * USDlong) - ETHlong;
+}
 
 function recalc(){
-//  outerMkt = askBFNX / bidGDAX;
-//  innerMkt = bidBFNX / askGDAX;
-//  console.log('bG:',bidGDAX,'aG:',askGDAX,'bB:',bidBFNX,'aB:',askBFNX,'in:',Number(innerMkt).toFixed(5),'out:',Number(outerMkt).toFixed(5));
-  console.log(bidGDAXBTC,askGDAXBTC,bidGDAXETH,askGDAXETH);
+  outerMkt = askGDAXETH / bidGDAXBTC;
+  innerMkt = bidGDAXETH / askGDAXBTC;
+  impBid = bidETHBTC(bidGDAXETHBTC,bidGDAXETH,askGDAXBTC);
+  impAsk = askETHBTC(askGDAXETHBTC,bidGDAXBTC,askGDAXETH);
+
+  console.log(bidGDAXBTC,askGDAXBTC,bidGDAXETH,askGDAXETH,Number(innerMkt).toFixed(6),Number(outerMkt).toFixed(6),
+Number(bidGDAXETHBTC).toFixed(6),Number(askGDAXETHBTC),impBid,impAsk);
 }
 
 orderBookMgrBFNX.on('bidUpdate',function(data){
@@ -64,7 +81,21 @@ orderBookMgrGDAXETH.on('askUpdate',function(data,security){
  }
 });
 
+orderBookMgrGDAXETHBTC.on('bidUpdate',function(data,security){
+  if(security == 'ETH-BTC'){
+    bidGDAXETHBTC = data;
+    recalc();
+  }
+});
+
+orderBookMgrGDAXETHBTC.on('askUpdate',function(data,security){
+  if(security == 'ETH-BTC'){
+    askGDAXETHBTC = data;
+    recalc();
+  }
+});
+
 handlerGDAXBTC.run();
 handlerGDAXETH.run();
 //handlerBFNX.run();
-
+handlerGDAXETHBTC.run();
